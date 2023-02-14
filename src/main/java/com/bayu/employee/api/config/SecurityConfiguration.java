de68c850d139d56d914917e0d34b830e92996140
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-14 12:55:18 +0700

    add Security Configuration

---
+++ src/main/java/com/bayu/employee/api/config/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.bayu.employee.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> {
+                            authorize.anyRequest().authenticated();
+                        }
+                )
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
+
+        return http.build();
+    }
+
+        @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails bayu = User.builder()
+                .username("bayu")
+                .password(passwordEncoder().encode("bayu123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(bayu, admin);
+    }
+
+}
