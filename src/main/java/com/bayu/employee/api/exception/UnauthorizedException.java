1ef073c5a23067349535a12469fcae32e81943c1
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-14 12:58:39 +0700

    add exceptions

---
+++ src/main/java/com/bayu/employee/api/exception/UnauthorizedException.java
@@ -0,0 +1,24 @@
+package com.bayu.employee.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException() {
+        super();
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnauthorizedException(Throwable cause) {
+        super(cause);
+    }
+}
