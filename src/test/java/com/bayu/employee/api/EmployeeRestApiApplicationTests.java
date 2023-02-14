ccd2d1b0948502eb85f7e2bedb0da2a762559479
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-14 10:07:56 +0700

    Initial commit

---
+++ src/test/java/com/bayu/employee/api/EmployeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.employee.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
